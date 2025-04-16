ff5a3a505c1f29fab4cc9b1d62af1a9d76451595
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-04-17 00:00:24 +0100

    setup css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <h1 className="text-5xl text-orange-500 font-bold">
+                Welcome!
+            </h1>
+        </main>
+    )
+}
+export default App
