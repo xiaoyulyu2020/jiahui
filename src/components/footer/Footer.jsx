2cdb27a2dde190b4be76c373fe02c70dac8b318c
xiaoyulyu2020 <lyu.xiaoyu2@mail.dcu.ie>
2025-04-22 07:59:35 +0100

    work

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12038.520597072616!2d117.2431!3d40.2063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35f052c9c25f199f%3A0x805c2e0d3b0a96dc!2z5Lit5Zu95YyX5Lqs5YyF5rW35aCC!5e0!3m2!1szh-CN!2sie!4v1713700000000!5m2!1szh-CN!2sie&markers=40.2063,117.2431"
                         width="100%"
                         height="200"
-                        style={{ border: 0 }}
+                        style={{ width: "100%", height: "100%", border: 0 }}
                         allowFullScreen=""
                         loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
